4db80039fcca704e3031994af702199be2427977
lala Feng <fenglala@outlook.com>
2018-03-20 22:10:57 +0800

    第一次提交

---
+++ app/src/main/java/com/jluandroid/myweather/fragments/ChooseAreaFragment.java
@@ -0,0 +1,15 @@
+package com.jluandroid.myweather.fragments;
+
+import android.app.ProgressDialog;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Fengl on 2018/3/20.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+    public static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+
+}
---
+++ app/src/main/java/com/jluandroid/myweather/util/Utility.java
@@ -0,0 +1,92 @@
+package com.jluandroid.myweather.util;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.jluandroid.myweather.db.City;
+import com.jluandroid.myweather.db.County;
+import com.jluandroid.myweather.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Fengl on 2018/3/20.
+ */
+
+public class Utility {
+
+    private static final String TAG = "Utility";
+
+    /**
+     * 解析和处理服务器返回的省级数据
+     */
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvinces = new JSONArray(response);
+                for (int i = 0; i < allProvinces.length(); i++) {
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(TAG, "handleProvinceResponse: JSON解析省级数据时出错啦");
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的市级数据
+     */
+    public static boolean handleCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(TAG, "handleCityResponse: JSON处理市级数据时出错啦");
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解析和处理服务器返回的县级数据
+     */
+    public static boolean handleCountyResponse(String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounties = new JSONArray(response);
+                for (int i = 0; i < allCounties.length(); i++) {
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e(TAG, "handleCountyResponse: JSON处理县级数据时出错啦");
+            }
+        }
+        return false;
+    }
+}
